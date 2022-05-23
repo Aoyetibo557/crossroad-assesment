3d6806f916963a00a7d0a4557827b8c09f92de23
aoyetibo557 <aoyetibo@gmail.com>
2022-05-23 14:49:21 -0400

    Created the commitBoard file. Also creaded the head div in app.tsx. The commitboard will get all the data regarding the commit history

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CommitBoard from './components/CommitBoard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='app__head'>
+        <h4 className='app__h4'>CrossRoads Assesment</h4>
+      </div>
+      <CommitBoard />
     </div>
   );
 }
---
+++ src/components/CommitBoard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CommitBoard = () => {
+  return (
+    <div>CommitBoard</div>
+  )
+}
+
+export default CommitBoard
